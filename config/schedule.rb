3703308d6c19ea1ee8affab48180881743829cd3
Esther Ng <esther.hc.ng@gmail.com>
2017-01-06 12:06:53 -0800

    One more time scheduler! After changing server time

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,7 +18,7 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
-every :friday, :at => '11:55am' do
+every :friday, :at => '12:10pm' do
   rake "purge:expired"
   rake "populate:qfc"
   rake "populate:safeway"

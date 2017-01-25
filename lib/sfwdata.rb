36622d301e898c691ae1151878367e378dfb8ecf
Esther Ng <esther.hc.ng@gmail.com>
2017-01-24 20:36:57 -0800

    Account for prices in cents

--- lib/sfwdata.rb
+++ lib/sfwdata.rb
@@ -43,11 +43,14 @@ private
     quantity = 1
     take = false
     word_array.each do |word|
+      if word.include?("\u00a2")
+        return price = (word[0..1]).to_i
+      end
       if word[0] == "$"
         price = ((word.delete("$").to_f)*100).to_i
       end
       if word[0].to_i != 0
-        quantity = word[0].to_i
+        quantity = word.to_i
       end
     end
     return price/quantity
@@ -61,3 +64,19 @@ end
 #   cat = categories[0]["CategoryName"].split(" ")
 #   if Category.
 # end
+# still need to handle these cases:
+=begin
+{
+      "Categories":[
+        {
+          "CategoryNameType":"Canned Goods & SoupsContent Categories","CategoryName":"Canned Goods & Soups","CategoryType":"Content Categories"
+        }
+      ],"EventId":"3013479","OfferId":"3935678","ItemType":null,"VersionId":"3013479","PageVariantId":"3042045","StoreEventId":"143F749033013479","OfferType":null,"PositionNumber":null,"DefaultImage":"C_3042045_14_0ec8cd64c295","HeadLine":"Signature Kitchens Ingredient Beans or Diced Tomatoes","BodyCopy":"14.5\u201315.5 oz Selected Varieties INDIVIDUAL ITEM PRICE: 79\u00a2 EA CASE PRICE: $5.88","SalePrice":"49\u00a2 EA. when you buy 12 with card","RegularPrice":null,"WasNowPrice":null,"UOM":null,"PriceCopy":"49\u00a2 EA. when you buy 12 with card","SaveAmount":null,"CallOutCopy":null,"LimitText":null,"LegalText":null,"StartDate":"2017-01-18T00:00:00","EndDate":"2017-01-24T23:59:59","ProductCode":null,"BarCode":null
+    },{
+      "Categories":[
+        {
+          "CategoryNameType":"Canned Goods & SoupsContent Categories","CategoryName":"Canned Goods & Soups","CategoryType":"Content Categories"
+        }
+      ],"EventId":"3013479","OfferId":"3935690","ItemType":null,"VersionId":"3013479","PageVariantId":"3042045","StoreEventId":"143F749033013479","OfferType":null,"PositionNumber":null,"DefaultImage":"C_3042045_26_21340da70b9a","HeadLine":"Nalley Chili","BodyCopy":"14 oz Selected Varieties INDIVIDUAL ITEM PRICE: 4 FOR $5.00 CASE PRICE: $21.12","SalePrice":"88\u00a2 EA. when you buy 24 with card","RegularPrice":null,"WasNowPrice":null,"UOM":null,"PriceCopy":"88\u00a2 EA. when you buy 24 with card","SaveAmount":null,"CallOutCopy":null,"LimitText":null,"LegalText":null,"StartDate":"2017-01-18T00:00:00","EndDate":"2017-01-24T23:59:59","ProductCode":null,"BarCode":null
+    }
+=end
